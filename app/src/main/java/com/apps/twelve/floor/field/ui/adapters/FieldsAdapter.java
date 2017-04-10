cb4f033f1a7fde484fa833c30d139faf8d21be7e
Yarick <rudnyk.yaroslav@gmail.com>
2017-04-10 11:19:54 +0300

    swipe deletion

--- app/src/main/java/com/apps/twelve/floor/field/ui/adapters/FieldsAdapter.java
+++ app/src/main/java/com/apps/twelve/floor/field/ui/adapters/FieldsAdapter.java
@@ -1,5 +1,7 @@
 package com.apps.twelve.floor.field.ui.adapters;
 
+import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +24,8 @@ import org.jetbrains.annotations.NotNull;
 
 public class FieldsAdapter extends RecyclerView.Adapter<FieldsAdapter.FieldViewHolder> {
 
+  private static final long AUTO_BACK_ANIMATION_DELAY = 2000L;
+
   private ArrayList<Field> mFieldsList = new ArrayList<>();
 
   @Override public FieldViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -31,24 +35,7 @@ public class FieldsAdapter extends RecyclerView.Adapter<FieldsAdapter.FieldViewH
   }
 
   @Override public void onBindViewHolder(FieldViewHolder holder, int position) {
-    Field field = mFieldsList.get(position);
-    holder.mNameText.setText(field.getName());
-    holder.mAreaText.setText(String.valueOf((int) field.getArea()));
-    holder.mDeleteBtn.setOnClickListener(v -> {
-      mFieldsList.remove(position);
-      notifyDataSetChanged();
-    });
-    holder.mSwipeActionView.setSwipeGestureListener(new SwipeGestureListener() {
-      @Override public boolean onSwipedLeft(@NotNull SwipeActionView swipeActionView) {
-        swipeActionView.moveToOriginalPosition(2000);
-        return false;
-      }
-
-      @Override public boolean onSwipedRight(@NotNull SwipeActionView swipeActionView) {
-        swipeActionView.moveToOriginalPosition(2000);
-        return false;
-      }
-    });
+    updateHolder(holder, position);
   }
 
   @Override public int getItemCount() {
@@ -64,11 +51,61 @@ public class FieldsAdapter extends RecyclerView.Adapter<FieldsAdapter.FieldViewH
     return mFieldsList.get(position);
   }
 
+  private void updateHolder(FieldViewHolder holder, int position) {
+    Field field = mFieldsList.get(position);
+
+    holder.mNameText.setText(field.getName());
+    holder.mAreaText.setText(String.valueOf((int) field.getArea()));
+
+    holder.mCancelBtn.setOnClickListener(v -> setBackgroundClickable(holder, false));
+    holder.mDeleteBtn.setOnClickListener(v -> {
+      setBackgroundClickable(holder, false);
+
+      // TODO: need to cancel animation on delete
+      holder.mSwipeActionView.clearAnimation(); // this doesn't work
+
+      mFieldsList.remove(position);
+      notifyDataSetChanged();
+    });
+
+    holder.mButtonsBackgroundConstraintLayout.setOnClickListener(
+        v -> setBackgroundClickable(holder, false));
+
+    holder.mSwipeActionView.setSwipeGestureListener(new SwipeGestureListener() {
+      @Override public boolean onSwipedLeft(@NotNull SwipeActionView swipeActionView) {
+        return onSwipe(holder, swipeActionView);
+      }
+
+      @Override public boolean onSwipedRight(@NotNull SwipeActionView swipeActionView) {
+        return onSwipe(holder, swipeActionView);
+      }
+    });
+
+    setBackgroundClickable(holder, false);
+  }
+
+  private boolean onSwipe(FieldViewHolder holder, SwipeActionView swipeActionView) {
+    swipeActionView.moveToOriginalPosition(AUTO_BACK_ANIMATION_DELAY);
+    (new Handler()).postDelayed(() -> setBackgroundClickable(holder, false),
+        AUTO_BACK_ANIMATION_DELAY);
+    setBackgroundClickable(holder, true);
+    return false;
+  }
+
+  private void setBackgroundClickable(FieldViewHolder holder, boolean isClickable) {
+    holder.mCancelBtn.setClickable(isClickable);
+    holder.mDeleteBtn.setClickable(isClickable);
+    holder.mButtonsBackgroundConstraintLayout.setClickable(isClickable);
+  }
+
   public static class FieldViewHolder extends RecyclerView.ViewHolder {
 
     @BindView(R.id.text_name) TextView mNameText;
     @BindView(R.id.text_area) TextView mAreaText;
     @BindView(R.id.button_delete) Button mDeleteBtn;
+    @BindView(R.id.button_cancel) Button mCancelBtn;
+    @BindView(R.id.constraint_layout_background) ConstraintLayout
+        mButtonsBackgroundConstraintLayout;
     @BindView(R.id.swipe_action_view) SwipeActionView mSwipeActionView;
 
     public FieldViewHolder(View itemView) {
