522eebd1f8803ba98bae84d1a18dcc49be2a39d2
Vrungel <nikita241296@gmail.com>
2017-06-21 13:39:32 +0300

    cropId -> crop Name

--- app/src/main/java/com/apps/twelve/floor/field/data/local/tables/conditions/ConditionsTable.java
+++ app/src/main/java/com/apps/twelve/floor/field/data/local/tables/conditions/ConditionsTable.java
@@ -12,7 +12,7 @@ public class ConditionsTable {
   @NonNull public static final String TABLE = "Conditions";
 
   @NonNull public static final String COLUMN_ID = "id";
-  @NonNull public static final String COLUMN_CROP_ID = "crop_id";
+  @NonNull public static final String COLUMN_CROP_NAME = "name";
   @NonNull public static final String COLUMN_PRIORITY = "priority";
   @NonNull public static final String COLUMN_CONDITION_TYPE_ID = "condition_type_id";
   @NonNull public static final String COLUMN_CONDITION_VALUE_ID = "condition_value_id";
@@ -29,10 +29,14 @@ public class ConditionsTable {
         + " ("
         + COLUMN_ID
         + " INTEGER NOT NULL PRIMARY KEY, "
-        + COLUMN_CROP_ID + " INTEGER, "
+        + COLUMN_CROP_NAME
+        + " TEXT, "
         + COLUMN_PRIORITY
         + " TEXT NULL, "
-        + COLUMN_CONDITION_TYPE_ID + " INTEGER, " + COLUMN_CONDITION_VALUE_ID + " INTEGER "
+        + COLUMN_CONDITION_TYPE_ID
+        + " INTEGER, "
+        + COLUMN_CONDITION_VALUE_ID
+        + " INTEGER "
         + ");";
   }
 }
