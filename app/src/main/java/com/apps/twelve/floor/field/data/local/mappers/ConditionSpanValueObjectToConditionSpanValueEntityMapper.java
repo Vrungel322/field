bdf65c0eae6be00c22af46143e2dac6248d247d1
Vrungel <nikita241296@gmail.com>
2017-06-21 13:38:36 +0300

    create conditions mappers

---
+++ app/src/main/java/com/apps/twelve/floor/field/data/local/mappers/ConditionSpanValueObjectToConditionSpanValueEntityMapper.java
@@ -0,0 +1,8 @@
+package com.apps.twelve.floor.field.data.local.mappers;
+
+/**
+ * Created by Vrungel on 21.06.2017.
+ */
+
+public class ConditionSpanValueObjectToConditionSpanValueEntityMapper implements Mapper {
+}
