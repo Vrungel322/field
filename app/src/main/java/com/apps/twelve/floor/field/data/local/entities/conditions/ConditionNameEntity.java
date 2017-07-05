8f891a29f609967c6d4d9e533435c41a5508b9fb
Vrungel <nikita241296@gmail.com>
2017-07-05 13:19:36 +0300

    ConditionNameEntity and obj add

--- app/src/main/java/com/apps/twelve/floor/field/data/local/entities/conditions/ConditionNameEntity.java
+++ app/src/main/java/com/apps/twelve/floor/field/data/local/entities/conditions/ConditionNameEntity.java
@@ -1,8 +1,62 @@
 package com.apps.twelve.floor.field.data.local.entities.conditions;
 
+import com.apps.twelve.floor.field.data.local.tables.conditions.ConditionNamesTable;
+import com.apps.twelve.floor.field.data.local.tables.conditions.WeedNutritionTypesTable;
+import com.pushtorefresh.storio.sqlite.annotations.StorIOSQLiteColumn;
+import com.pushtorefresh.storio.sqlite.annotations.StorIOSQLiteType;
+
 /**
  * Created by Vrungel on 05.07.2017.
  */
-
+@StorIOSQLiteType(table = ConditionNamesTable.TABLE)
 public class ConditionNameEntity {
+  @StorIOSQLiteColumn(name = ConditionNamesTable.COLUMN_ID, key = true) Long id;
+  @StorIOSQLiteColumn(name = ConditionNamesTable.COLUMN_NAME) String name;
+
+  public ConditionNameEntity() {
+  }
+
+  public ConditionNameEntity(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public static ConditionNameEntity newConditionNameEntity(Long id, String name) {
+    if (id == 0) id = null;
+    return new ConditionNameEntity(id, name);
+  }
+
+  @Override public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+
+    ConditionNameEntity that = (ConditionNameEntity) obj;
+
+    if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+    return name != null ? name.equals(that.name) : that.name == null;
+  }
+
+  @Override public int hashCode() {
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+
+    return result;
+  }
 }
--- app/src/main/java/com/apps/twelve/floor/field/data/local/objects/conditions/ConditionNameObject.java
+++ app/src/main/java/com/apps/twelve/floor/field/data/local/objects/conditions/ConditionNameObject.java
@@ -1,8 +1,59 @@
 package com.apps.twelve.floor.field.data.local.objects.conditions;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.annotation.NonNull;
+
 /**
  * Created by Vrungel on 05.07.2017.
  */
 
-public class ConditionNameObject {
+public class ConditionNameObject implements Parcelable {
+  private long mId;
+  @NonNull private String mName;
+
+  public ConditionNameObject(long id, @NonNull String name) {
+    mId = id;
+    mName = name;
+  }
+
+  public long getId() {
+    return mId;
+  }
+
+  public void setId(long id) {
+    mId = id;
+  }
+
+  @NonNull public String getName() {
+    return mName;
+  }
+
+  public void setName(@NonNull String name) {
+    mName = name;
+  }
+
+  protected ConditionNameObject(Parcel in) {
+    mId = in.readLong();
+    mName = in.readString();
+  }
+
+  @Override public void writeToParcel(Parcel dest, int flags) {
+    dest.writeLong(mId);
+    dest.writeString(mName);
+  }
+
+  @Override public int describeContents() {
+    return 0;
+  }
+
+  public static final Creator<ConditionNameObject> CREATOR = new Creator<ConditionNameObject>() {
+    @Override public ConditionNameObject createFromParcel(Parcel in) {
+      return new ConditionNameObject(in);
+    }
+
+    @Override public ConditionNameObject[] newArray(int size) {
+      return new ConditionNameObject[size];
+    }
+  };
 }
